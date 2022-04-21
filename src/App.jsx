070b33d15fef925f0f0fed3a582ff4d42d13711d
erick.deras <erick.deras@my.unitec.edu.mx>
2022-04-20 23:42:13 -0500

    MScondCommit

--- src/App.jsx
+++ src/App.jsx
@@ -4,40 +4,44 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [linea1, setLinea1] = useState([]);
+  const [linea2, setLinea2] = useState([]);
+  const [memeImage, setMemeImage] = useState([]);
 
+  const onChangeLinea1 = function (evento) {
+    setLinea1(evento.target.value)
+  }
+
+  const onChangeLinea2 = function (evento) {
+    setLinea2(evento.target.value)
+  }
+
+  const onChangeMemeImage = function (evento) {
+    setMemeImage(evento.target.value)
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <select onChange={onChangeMemeImage}>
+        <option value="CasaEnLlamas">Casa en llamas</option>
+        <option value="history">History Channel</option>
+        <option value="matrix">Matrix</option>
+        <option value="philosoraptor">Filosoraptor</option>
+      </select><br />
+      {/* Select picker meme */}
+      {/* Input text - Primera linea
+      Input text - segunda linea
+      Boton exportar */}
+      <input onChange={onChangeLinea1} type="text" placeholder='Linea 1'/><br />
+      <input onChange={onChangeLinea2} type="text" placeholder='Linea 2'/><br />
+      <button>Exportar</button>
+
+      <div>
+        <span>{linea1}</span><br />
+        <span>{linea2}</span><br />
+        <img src={"./img/" + memeImage + ".jpg"} alt="memeImage" />
+      </div>
+
     </div>
   )
 }
